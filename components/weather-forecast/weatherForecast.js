c816b9346dbbfe7d8e592119106bb7219c7865d0
Will Morrissey <wtm.41691@gmail.com>
2019-06-17 20:10:04 -0400

    Style adjustments to weather container and nav

--- components/weather-forecast/weatherForecast.js
+++ components/weather-forecast/weatherForecast.js
@@ -18,7 +18,7 @@ console.log(true);
 angular.module('WeatherEventApp')
   .component('weatherForecast', {
     template: `
-    <nav class="nav-container">
+    <div class="nav-container">
 
     <div>
       <a id="show" class="logo" href="#!/weather-list">WeatherBrite GR</a>
@@ -29,10 +29,10 @@ angular.module('WeatherEventApp')
       <li class="nav-item"><a href="#!/event-details"><span style="color: rgb(240, 45, 38)">❤</span> Favorites</a></li>
     </ul>
     <span class = "forecast">Extended Forecast: </span>
-    <div class = "forecast" ng-repeat="item in $ctrl.weatherData.list">{{item.main.temp}}</div>
+    <div class = "forecast" ng-repeat="item in $ctrl.weatherData.list">{{item.main.temp | number:0}} ℉ </div>
   
 
-    </nav>
+    </div>
 
 
 
