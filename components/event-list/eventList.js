ecf8329432c928de856b214bc8329220cd7751af
Humberto Flores <hflores@nhaschools.com>
2019-06-26 22:17:19 -0400

    adding width to the drop down

--- components/event-list/eventList.js
+++ components/event-list/eventList.js
@@ -113,14 +113,14 @@ angular.module('WeatherEventApp')
 
 
   <div class="filter-category">
-  <select   ng-model="selectedCategory" ng-change= "$ctrl.selectCategory(selectedCategory)" class="custom-select" style="width:9vw; height:6vh; color:white; background-color:#2E4057; border:none; font-size:0.9em;">
+  <select   ng-model="selectedCategory" ng-change= "$ctrl.selectCategory(selectedCategory)" class="custom-select" style="width:13vw; height:6vh; color:white; background-color:#2E4057;  font-size:0.9em;">
     <option value="" selected="selected">Category</option>
     <option ng-repeat="item in $ctrl.eventCategories.categories" value="{{item.id}}">{{item.name}}</option>
   </select>
 </div>
 
   <div class="filter-category">
-    <select ng-model="priceSelection" class="custom-select" ng-change="$ctrl.selectPrice(priceSelection)" style="width:9vw; height:6vh; color:white; background-color:#2E4057; border:none; font-size:0.9em;">
+    <select ng-model="priceSelection" class="custom-select" ng-change="$ctrl.selectPrice(priceSelection)" style="width:13vw; height:6vh; color:white; background-color:#2E4057;  font-size:0.9em;">
       <option value="" selected="selected">Price</option>  
       <option value="free" selected="free">Free Events</option>
       <option value="paid" selected="paid">Paid Events</option>
